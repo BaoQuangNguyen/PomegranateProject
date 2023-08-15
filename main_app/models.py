5774a7e8ef2258c97c6f1073dcbed4d5342792a2
BaoQuangNguyen <bnguyen0239@gmail.com>
2023-08-15 18:10:32 -0500

    refactor

--- main_app/models.py
+++ main_app/models.py
@@ -1,38 +1,16 @@
 from django.db import models
 from django.urls import reverse
-# Create your models here.
 
-PRIORITY_OPTIONS = (
-    ('L', 'Low'),
-    ('M', 'Medium'),
-    ('H', 'High'),
-)
 class Task(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=300)
     due_date = models.DateTimeField()
-    notes = models.TextField(blank=True)
-    completed_at = models.DateTimeField(null=True, blank=True)
     total_time_spent = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.title
-    
-    def get_absolute_url(self):
-        return reverse('detail', kwargs={'task_id': self.id})
-    
-class Status(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     complete = models.BooleanField(default=False)
-    pomegranates_completed = models.IntegerField(default=0)
-    breaks = models.IntegerField(default=0)
-    task = models.ForeignKey(Task, on_delete=models.CASCADE)
-    is_latest = models.BooleanField(default=False)  # New field to indicate if it's the latest status
 
     def __str__(self):
-        return f"Created: {self.created}, Complete: {self.complete}, Pomegranates Completed: {self.pomegranates_completed}, Breaks: {self.breaks}"
-    
-    class Meta:
-        ordering = ['-created']
+        return f'{self.title} ({self.id})'
 
-
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'task_id': self.id})
--- main_app/urls.py
+++ main_app/urls.py
@@ -8,8 +8,6 @@ urlpatterns = [
     path('tasks/create/', views.TaskCreate.as_view(), name='tasks_create'),
     path('tasks/<int:pk>/update/', views.TaskUpdate.as_view(), name='tasks_update'),
     path('tasks/<int:pk>/delete/', views.TaskDelete.as_view(), name='tasks_delete'),
-    path('tasks/<int:task_id>/update_status/', views.update_status, name='update_status'),
     path('timer/', views.timer, name='timer'),
-    path('tasks/<int:task_id>/results/', views.task_results, name='task_results'),
     path('breaktimer/', views.break_timer, name='breaktimer'),
 ]
